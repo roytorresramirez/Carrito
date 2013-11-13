afc1a85578aa5223d9122248ea8ff425333cd648
Manuel Del Pozo <manueldelpozo29@gmail.com>
2013-11-13 14:55:21 -0500

    avance de la interfaz

--- carrito/app/models.py
+++ carrito/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+	titulo = models.CharField(max_length = 140)
+
+	def __unicode__(self):
+		return self.titulo
+
+class Item(models.Model):
+	nombre = models.CharField(max_length = 40)
+	descripción = models.CharField(max_length = 70)
+	precio = models.
